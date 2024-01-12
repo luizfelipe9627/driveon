7e554fa37b6d0fabde8e6dca744f02070de2a1ce
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-01-12 03:15:10 -0300

    update

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,8 @@
-// Está importando o módulo scroll-animation.js e atribuindo a variável AnimationScroll.
 import AnimationScroll from "./modules/scroll-animation.js";
 import MenuMobile from "./modules/menu-mobile.js";
 
-const animationScroll = new AnimationScroll("[data-anime='scroll']"); // Está criando um objeto e passando o dataset anime="scroll" como parâmetro.
-animationScroll.init(); // Está chamando o método init do objeto criado.
+const animationScroll = new AnimationScroll("[data-anime='scroll']");
+animationScroll.init()
 
-const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']"); // Está criando um objeto e passando o dataset menu="button" e menu="list" como parâmetro.
-menuMobile.init(); // Está chamando o método init do objeto criado.
+const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
+menuMobile.init();
