3fa604acfcf87d40df63dbea9d58624342469117
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-01-12 03:14:42 -0300

    update

--- src/scripts/modules/menu-mobile.js
+++ src/scripts/modules/menu-mobile.js
@@ -1,48 +1,61 @@
+// Classe responsável por fazer o menu mobile abrir e fechar.
+// O export é usado para permitir que o código seja usado em outro arquivo JS. O default é geralmente usado para quando tem que exportar somente uma função/classe do mesmo arquivo.
 export default class MenuMobile {
+  // O constructor é usado para definir os parâmetros que serão usados na classe.
   constructor(menuButton, menuList, events) {
-    this.menuButton = document.querySelector(menuButton);
-    this.menuList = document.querySelector(menuList);
-    this.activeClass = "active";
-    this.openMenu = this.openMenu.bind(this);
+    this.menuButton = document.querySelector(menuButton); // Atribui ao this.menuButton o elemento passado pelo usuário no parâmetro menuButton.
+    this.menuList = document.querySelector(menuList); // Atribui ao this.menuList o elemento passado pelo usuário no parâmetro menuList.
+    this.activeClass = "active"; // Está definindo o valor padrão para o activeClass.
+    this.openMenu = this.openMenu.bind(this); // Está atribuindo a função openMenu a variável openMenu e passando como parâmetro o bind(this) para que o this não seja perdido ao usar o addEventListener.
 
+    // Se o events for undefined, ou seja, se o usuário não passar nenhum parâmetro, então irá executar o if.
     if (events === undefined) {
-      this.events = ["touchstart", "click"];
+      this.events = ["touchstart", "click"]; // Está atribuindo a variável events um array com os eventos touchstart e click.
     } else {
-      this.events = events;
+      this.events = events; // Está atribuindo a variável events o parâmetro passado pelo usuário.
     }
   }
 
+  // O método openMenu é responsável por abrir e fechar o menu mobile.
   openMenu(event) {
-    event.preventDefault();
-    this.menuButton.classList.toggle(this.activeClass);
-    this.menuList.classList.toggle(this.activeClass);
-    this.scrollDisable();
+    event.preventDefault(); // Está prevenindo o comportamento padrão do evento.
+    this.menuButton.classList.toggle(this.activeClass); // Está adicionando a classe active ao menuButton e se já tiver a classe active irá remover.
+    this.menuList.classList.toggle(this.activeClass); // Está adicionando a classe active ao menuList e se já tiver a classe active irá remover.
+    this.scrollDisable(); // Está chamando o método scrollDisable, fazendo com que o usuário não consiga rolar a tela quando o menu mobile estiver aberto.
   }
 
+  // O método addMenuMobileEvents está sendo usado para adicionar os eventos ao menu mobile.
   addMenuMobileEvents() {
+    // O forEach está sendo usado para percorrer cada item do array. O parâmetro event do forEach está sendo usado para referenciar cada item do array.
     this.events.forEach((event) => {
-      this.menuButton.addEventListener(event, this.openMenu);
+      this.menuButton.addEventListener(event, this.openMenu); // Está adicionando o evento ao menuButton e se houver o evento aciona o método openMenu.
     });
   }
 
+  // O método scrollDisable está sendo usado para fazer com que o usuário não consiga rolar a tela quando o menu mobile estiver aberto.
   scrollDisable() {
+    // Se o menuButton conter a classe active irá executar o if.
     if (this.menuButton.classList.contains(this.activeClass)) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = "hidden"; // Está adicionando a propriedade overflow com o valor hidden ao body, fazendo com que o usuário não consiga rolar a tela.
     } else {
-      document.body.style.overflow = "initial";
+      document.body.style.overflow = "initial"; // Está adicionando a propriedade overflow com o valor initial ao body, fazendo com que o usuário consiga rolar a tela.
     }
   }
 
+  // O init está sendo usado para iniciar o objeto criado.
   init() {
+    // Se o menuButton e o menuList existirem irá executar o if.
     if (this.menuButton && this.menuList) {
-      this.addMenuMobileEvents();
+      this.addMenuMobileEvents(); // Está chamando o método addMenuMobileEvents para adicionar os eventos ao menu mobile.
     }
     return this;
   }
 
+  // O stop está sendo usado para parar o objeto criado.
   stop() {
+    // O forEach está sendo usado para percorrer cada item do array. O parâmetro event do forEach está sendo usado para referenciar cada item do array.
     this.events.forEach((event) => {
-      this.menuButton.removeEventListener(event, this.openMenu);
+      this.menuButton.removeEventListener(event, this.openMenu); // Está removendo o evento ao menuButton e se houver o evento aciona o método openMenu.
     });
   }
 }
--- src/scripts/modules/scroll-animation.js
+++ src/scripts/modules/scroll-animation.js
@@ -1,44 +1,60 @@
-import debounce from "./debounce.js";
+import debounce from "./debounce.js"; // Importa a função debounce do arquivo debounce.js.
 
+// Classe responsável por fazer conteúdo aparecer animado ao rolar o scroll e abrir o site.
+// O export é usado para permitir que o código seja usado em outro arquivo JS. O default é geralmente usado para quando tem que exportar somente uma função/classe do mesmo arquivo.
 export default class AnimationScroll {
+  // O constructor é usado para definir os parâmetros que serão usados na classe.
   constructor(sections) {
-    this.sections = document.querySelectorAll(sections);
-    this.windowHalf = window.innerHeight * 0.7;
-    this.activeClass = "active";
-    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
+    this.sections = document.querySelectorAll(sections); // Atribui ao this.sections os elementos passados pelo usuário.
+    this.windowHalf = window.innerHeight * 0.7; // Está pegando 70% da altura da tela do usuário.
+    this.activeClass = "active"; // Está definindo o valor padrão para o activeClass.
+
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50); // Está atribuindo a função debounce a variável checkDistance e passando como parâmetro a função checkDistance e o tempo de 200 milissegundos. O bind(this) está sendo usado para que o this não seja perdido ao usar o debounce.
   }
 
+  // O método getDistance está sendo usado para pegar a altura do elemento em relação ao topo da página.
   getDistance() {
+    // O this.distance está sendo usado para referenciar o objeto que está sendo criado, sendo assim, o this.sections está se referindo aos elementos passados pelo usuário. O this.section está sendo transformado em um array e está sendo usado o map para percorrer cada section.
     this.distance = [...this.sections].map((section) => {
-      const offSet = section.offsetTop;
+      const offSet = section.offsetTop; // Está pegando a altura do elemento em relação ao topo da página.
+      // Está retornando um objeto com o elemento e a altura do elemento em relação ao topo da página.
       return {
-        element: section,
-        offSet: Math.floor(offSet - this.windowHalf),
+        element: section, // Está retornando o elemento.
+        offSet: Math.floor(offSet - this.windowHalf), // Está retornando a altura do elemento em relação ao topo da página.
       };
     });
   }
 
+  // O método checkDistante é responsável por fazer os elementos aparecerem na tela ao scrollar para baixo.
   checkDistance() {
+    // O forEach está sendo usado para percorrer cada item do array.  parâmetro item do forEach está sendo usado para referenciar cada item do array.
     this.distance.forEach((item) => {
+      // Se o window.scrollY for maior que a altura do elemento em relação ao topo da página, então irá executar o if.
       if (window.scrollY > item.offSet) {
-        item.element.classList.add(this.activeClass);
-      } else if (item.element.classList.contains(this.activeClass)) {
-        item.element.classList.remove(this.activeClass);
+        item.element.classList.add(this.activeClass); // Adiciona a classe active a section que tiver sua altura negativada(de cima para baixo).
+      }
+      // Se não, se a section conter a classe active irá executar o escopo do else if que é responsável por fazer os elementos sumirem da tela ao scrollar para cima.
+      else if (item.element.classList.contains(this.activeClass)) {
+        item.element.classList.remove(this.activeClass); // Remove a classe active a section que tiver sua altura positiva(de baixo para cima).
       }
     });
   }
 
+  // O init está sendo usado para iniciar o objeto criado.
   init() {
+    // Se o this.sections.length for maior que 0, então irá executar o if.
     if (this.sections.length) {
-      this.getDistance();
-      this.checkDistance();
+      this.getDistance(); // Está executando a função para que assim que o site carregue não fique tudo em branco.
 
-      window.addEventListener("scroll", this.checkDistance);
+      this.checkDistance(); // Está executando a função para que assim que o site carregue não fique tudo em branco.
+
+      window.addEventListener("scroll", this.checkDistance); // Adiciona o evento de scroll ao window e se houver scroll aciona o método checkDistance.
     }
-    return this;
+    return this; // Está retornando o objeto criado para permitir a que o init possa usar ou acessar outros métodos da classe.
   }
 
+  // O stop está sendo usado para parar o objeto criado.
   stop() {
-    window.removeEventListener("scroll", this.checkDistance);
+    window.removeEventListener("scroll", this.checkDistance); // Remove o evento de scroll ao window e se houver scroll aciona o método checkDistance.
   }
 }
